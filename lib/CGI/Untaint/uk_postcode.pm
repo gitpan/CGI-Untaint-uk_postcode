778b8174d15aea7911fdeace899e6f8964ca7345
Tony Bowden <tony@tmtm.com>
2005-09-20 21:23:21 +0000

    Import of TMTM/CGI-Untaint-uk_postcode-1.00 from CPAN.
    gitpan-cpan-distribution: CGI-Untaint-uk_postcode
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         TMTM/CGI-Untaint-uk_postcode-1.00.tar.gz
    gitpan-cpan-author:       TMTM
    gitpan-cpan-maturity:     released

--- lib/CGI/Untaint/uk_postcode.pm
+++ lib/CGI/Untaint/uk_postcode.pm
@@ -1,18 +1,19 @@
 package CGI::Untaint::uk_postcode;
 
+$VERSION = '1.00';
+
 use strict;
 use base 'CGI::Untaint::object';
 
-use vars qw/$VERSION/;
-$VERSION = '0.02';
 
 sub _untaint_re { 
-  my @patterns = ('AN NAA', 'ANN NAA', 'AAN NAA', 'AANN NAA',
-                  'ANA NAA', 'AANA NAA', 'AAA NAA');
+  my @patterns = ('ON NII', 'ONN NII', 'OON NII', 'OONN NII',
+                  'ONO NII', 'OONO NII', 'OOO NII');
 
   foreach (@patterns) {
-    s/A/[A-Z]/g;
     s/N/\\d/g;
+    s/O/[A-Z]/g; # outward code
+    s/I/[ABDEFGHJLNPQRSTUWXYZ]/g; # inward code
     s/ /\\s?/g;
   }
 
@@ -34,24 +35,41 @@ CGI::Untaint::uk_postcode - validate a UK postcode
 =head1 DESCRIPTION
 
 This CGI::Untaint input handler verifies that it is dealing with a
-reasonable UK postcode, as defined by the Royal Mail. It does not check
-that the postcode is valid.
+reasonably plausible UK postcode, according to some checks by the Royal
+Mail. 
+
+Due to the complexities of the UK postal code system it is impossible to
+accurately check if the postcode is real, or even if it is of a completely
+valid format. As such there may be false positives. There should not,
+however, be any false negatives, so if you find any valid postcodes that
+this rejects, PLEASE let me know.
 
 =head1 SEE ALSO
+
+http://en.wikipedia.org/wiki/Postcode
 
 L<CGI::Untaint>
 
 =head1 AUTHOR
 
-Tony Bowden, E<lt>kasei@tmtm.comE<gt>. Regular expression from
-Craig Berry 
+Tony Bowden. Based on original regular expression by Craig Berry.
+
+=head1 BUGS and QUERIES
+
+Please direct all correspondence regarding this module to:
+  bug-CGI-Untaint-uk_postcode@rt.cpan.org
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001 Tony Bowden. All rights reserved.
+  Copyright (C) 2001-2005 Tony Bowden.
 
-This module is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License; either version 2 of the License,
+  or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.
 
 =cut
 
